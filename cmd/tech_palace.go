0bc5f1d8b9fb1fccb1f7c02db67bd93481a07c87
jeremie giffard <e132784f@gmail.com>
2023-04-23 12:29:02 +0200

    tech palace AddBorder() method

--- cmd/tech_palace.go
+++ cmd/tech_palace.go
@@ -2,25 +2,34 @@ package cmd
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/spf13/cobra"
 )
 
 var techPalace = &cobra.Command{
-	Use:   "techPalace [--name=<random name>]",
-	Short: "Print techPalace --name=<random name>",
+	Use:   "techPalace [--name=<random name>] [--stars=<Integer>]",
+	Short: "Print techPalace --name=<random name> [--stars=<Integer>]",
 	Long:  `All software has versions. techPalace's`,
 	Run: func(cmd *cobra.Command, args []string) {
 		name, _ := cmd.Flags().GetString("name")
-		WelcomeMessage(name)
+		stars, _ := cmd.Flags().GetInt("stars")
+		//
+		WelcomeMessage(name, stars)
+
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(techPalace)
 	techPalace.PersistentFlags().String("name", "", "Name of visitor")
+	techPalace.PersistentFlags().Int("stars", 0, "number of stars")
 }
 
-func WelcomeMessage(name string) {
-	fmt.Printf("Welcome to the Tech Palace, %s", name)
+func WelcomeMessage(name string, stars int) {
+	Premium := AddBorder(stars)
+	fmt.Printf("%s\nWelcome to the Tech Palace, %s\n%s", Premium, name, Premium)
+}
+func AddBorder(stars int) string {
+	return strings.Repeat("*", stars)
 }
